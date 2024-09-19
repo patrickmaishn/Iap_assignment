873606e82c62c1a8ad2edab88d0f2fc9248cf90b
patrickmaishn <patricknmaina0@gmail.com>
2024-09-19 18:27:16 +0300

    user registration

--- Sign Up/display.php
+++ Sign Up/display.php
@@ -8,5 +8,11 @@ class database {
 
     public function getConnection(){
        $this->conn = null;
+       try {
+        $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       }catch(PDOException $exception) {
+        echo "Connection error: " . $exception->getMessage();
+           }
     }
 }
