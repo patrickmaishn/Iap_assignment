08b04461949660b530028ba20cede8caec2e62b2
patrickmaishn <patricknmaina0@gmail.com>
2024-09-20 23:03:33 +0300

    Registration

--- Sign Up/signup.php
+++ Sign Up/signup.php
@@ -34,6 +34,25 @@ class user{
     }
     public function createUser(){
         $query = "INSERT INTO" . $this->table_name . " (fullname, username, email, password, genderId, roleId) VALUES (:fullname, :username, :email, :password, :genderId, :roleId)";
+        $stmt = $this->conn->prepare($query);
+        $this->fullname = htmlspecialchars(strip_tags($this->fullname));
+        $this->username = htmlspecialchars(strip_tags($this->username));
+        $this->email = htmlspecialchars(strip_tags($this->email));
+        $this->password = password_hash($this->password, PASSWORD_BCRYPT); // Hash password for security
+        $this->genderId = htmlspecialchars(strip_tags($this->genderId));
+        $this->roleId = htmlspecialchars(strip_tags($this->roleId));
+
+        $stmt->bindParam(":fullname", $this->fullname);
+        $stmt->bindParam(":username", $this->username);
+        $stmt->bindParam(":email", $this->email);
+        $stmt->bindParam(":password", $this->password);
+        $stmt->bindParam(":genderId", $this->genderId);
+        $stmt->bindParam(":roleId", $this->roleId);
+
+        if($stmt->execute()){
+            return true;
+        }
+        return false;
     }
 
 }
