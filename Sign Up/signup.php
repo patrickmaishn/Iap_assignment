157587e26a3c56e93e8f8021faa3fd7b89721bbc
patrickmaishn <patricknmaina0@gmail.com>
2024-09-24 18:01:53 +0300

    User Registration

--- Sign Up/signup.php
+++ Sign Up/signup.php
@@ -74,3 +74,14 @@ if($_POST) {
 
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
