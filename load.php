58243018a2cbafab19919d5ce8cd8065e86a551d
patrickmaishn <patricknmaina0@gmail.com>
2024-09-28 11:32:11 +0300

    Brought in

---
+++ load.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require "includes/constants.php";
+require "includes/dbConnection.php";
+
+// Class Auto Load 
+
+function classAutoLoad($classname){
+
+    $directories = ["contents", "layouts", "menus", "forms", "processes", "global"];
+
+    foreach($directories AS $dir){
+        $filename = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $classname . ".php";
+        if(file_exists($filename) AND is_readable($filename)){
+            require_once $filename;
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoad');
+
+    $ObjGlob = new flc();
+
+// Create instances of all classes
+    $ObjLayouts = new layouts();
+    $ObjMenus = new menus();
+    $ObjHeadings = new headings();
+    $ObjCont = new contents();
+    $ObjForm = new user_forms();
+    $conn = new dbConnection(DBTYPE, HOSTNAME, DBPORT, HOSTUSER, HOSTPASS, DBNAME);
+
+
+// Create process instances
+
+    $ObjAuth = new auth();
+    $ObjAuth->signup($conn, $ObjGlob);
