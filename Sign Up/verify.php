1a2b153dbc7446ecd44e2f4bf959baede46d78b5
patrickmaishn <patricknmaina0@gmail.com>
2024-10-16 13:37:44 +0300

    Modified background

--- Sign Up/verify.php
+++ Sign Up/verify.php
@@ -36,6 +36,12 @@ if($_POST) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Verify OTP</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        body{
+            background-color: lightslategray;
+        }
+    </style>
 </head>
 <body>
 <div class="container mt-5">
