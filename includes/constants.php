affa794fbbaf105a69b15c15d5e626538af3605c
patrickmaishn <patricknmaina0@gmail.com>
2024-11-07 11:51:41 +0300

    Pass 'NO'

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
 //creating constants
 define('DBTYPE', 'MySQLi');
 define('HOSTNAME', 'localhost');
-define('DBPORT', '3306');
+define('DBPORT', '3307');
 define('HOSTUSER', 'root');
 define('HOSTPASS', '');
 define('DBNAME', 'iap_d');
